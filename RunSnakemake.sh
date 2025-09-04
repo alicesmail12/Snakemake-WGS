673eddc730cc6305d2b2dbed1b3cd7f01eb16e3d
Alice Smail <114085896+alicesmail12@users.noreply.github.com>
2025-09-04 11:29:07 +0100

    Update RunSnakemake.sh

--- RunSnakemake.sh
+++ RunSnakemake.sh
@@ -11,7 +11,7 @@ if [ ! -d ./VCF-Output/ ]; then mkdir -p ./VCF-Output/; fi
 if [ ! -d ./VEP-Output/ ]; then mkdir -p ./VEP-Output/; fi
 
 # Load modules
-module load snakemake OpenBLAS/0.3.17-GCC-11.2.0 ncurses/6.4-GCCcore-12.3.0 SAMtools BWA/0.7.17-foss-2017b picard/2.20.6-Java-1.8.0_144
+module load snakemake OpenBLAS ncurses SAMtools BWA picard
 
 # Snakemake
 snakemake -c12 --rerun-incomplete
